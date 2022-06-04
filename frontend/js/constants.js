848830d8dee6b2754ee8790e13d9b2725a04b5d7
Jay Sloatman <jay@industrypbx.com>
2022-06-03 17:54:09 -0700

    metamask

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -2,8 +2,8 @@ const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contrac
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Isle of Plumii NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Isle of Plumii NFT Project is a decentralized Leading the way to the Metaverse';
+const welcome_h2 = "Connect to <a href='https://metamask.io'>MetaMask</a> to Get Started";
+const welcome_p = 'The Isle of Plumii NFT Project is a decentralized. Leading the way to the Metaverse';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
